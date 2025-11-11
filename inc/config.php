457c82fce73f5fe0ea0f7a88373c130bd00da656
Miguel Moreno <mmd.-@hotmail.com>
2025-11-10 19:37:18 -0600

    se actualiza la api para que funcione con ngrok

--- inc/config.php
+++ inc/config.php
@@ -4,4 +4,4 @@ define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
 define("DB_DATABASE_NAME", "db_fifaapp");
 define("DB_PORT", "3306");
-define("API_BASE_URL", "http://192.168.100.13/BACKEND_PROCESAMIENTO_IMAGENES");
+define("API_BASE_URL", "https://gapless-thirdly-jovita.ngrok-free.dev/BACKEND_PROCESAMIENTO_IMAGENES");
